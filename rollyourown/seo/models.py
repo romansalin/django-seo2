f902173fa26c0cbd679f43c9b6b71c3e35e62f07
Roman Salin <romansalin1990@gmail.com>
2015-04-27 16:11:58 +0300

    Fix loading default

--- rollyourown/seo/models.py
+++ rollyourown/seo/models.py
@@ -7,16 +7,18 @@ from django.conf import settings
 
 
 def setup():
+    is_loaded = False
     # Look for Metadata subclasses in appname/seo.py files
     for app in settings.INSTALLED_APPS:
         try:
             module_name = '%s.seo' % str(app)
             importlib.import_module(module_name)
+            is_loaded = True
         except ImportError:
             pass
 
     # if SEO_MODELS is defined, create a default Metadata class
-    if hasattr(settings, 'SEO_MODELS'):
+    if hasattr(settings, 'SEO_MODELS') and not is_loaded:
         importlib.import_module('rollyourown.seo.default')
 
     from rollyourown.seo.base import register_signals
