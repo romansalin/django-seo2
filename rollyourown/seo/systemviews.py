00bdf7153bfb7f5d6a95e4912aa6dff7ae80c857
Roman Salin <romansalin1990@gmail.com>
2015-04-28 12:15:02 +0300

    Cleanup

--- rollyourown/seo/systemviews.py
+++ rollyourown/seo/systemviews.py
@@ -4,27 +4,10 @@
 import importlib
 
 from django.apps import apps
-from django.db.models.fields import BLANK_CHOICE_DASH
-from django.db import models
-from django.utils.text import capfirst
-from django.utils.functional import lazy
-from django import forms
-
-from rollyourown.seo.utils import LazyChoices
 
 
 def get_seo_views(metadata_class):
     return get_view_names(metadata_class._meta.seo_views)
-
-    ## The following is a previous attempt to dynamically get all urls
-    ## This has a number of difficult spots, and is unnecessary when 
-    ## seo_views is given
-    #choices = SystemViews()
-    #seo_views = get_view_names(metadata_class._meta.seo_views)
-    #if seo_views:
-    #    return filter(lambda c: c[0] in seo_views, choices)
-    #else:
-    #    return choices
 
 
 def get_view_names(seo_views):
@@ -46,75 +29,3 @@ def get_view_names(seo_views):
                     if getattr(url, 'name', None):
                         output.append(url.name)
     return output
-
-
-class SystemViews(LazyChoices):
-    def populate(self):
-        """ Populate this list with all views that take no arguments.
-        """
-        from django.conf import settings
-        from django.core import urlresolvers
-
-        self.append(("", ""))
-        urlconf = settings.ROOT_URLCONF
-        resolver = urlresolvers.RegexURLResolver(r'^/', urlconf)
-        # Collect base level views
-        for key, value in resolver.reverse_dict.items():
-            if isinstance(key, basestring):
-                args = value[0][0][1]
-                url = "/" + value[0][0][0]
-                self.append((key, " ".join(key.split("_"))))
-        # Collect namespaces (TODO: merge these two sections into one)
-        for namespace, url in resolver.namespace_dict.items():
-            for key, value in url[1].reverse_dict.items():
-                if isinstance(key, basestring):
-                    args = value[0][0][1]
-                    full_key = '%s:%s' % (namespace, key)
-                    self.append((full_key, "%s: %s" % (namespace, " ".join(key.split("_")))))
-        self.sort()
-
-
-class SystemViewChoiceField(forms.TypedChoiceField):
-    def _get_choices(self):
-        return self._choices
-
-    def _set_choices(self, value):
-        self._choices = self.widget.choices = value
-
-    choices = property(_get_choices, _set_choices)
-
-
-class SystemViewField(models.CharField):
-    def __init__(self, restrict_to, *args, **kwargs):
-        self.restrict_to = restrict_to
-        kwargs.setdefault('max_length', 255)
-        kwargs.setdefault('choices', SystemViews())
-        super(SystemViewField, self).__init__(*args, **kwargs)
-
-    def get_choices(self, include_blank=True, blank_choice=BLANK_CHOICE_DASH):
-        return self.choices
-
-    def formfield(self, **kwargs):
-        defaults = {'required': not self.blank, 'label': capfirst(self.verbose_name), 'help_text': self.help_text}
-        if self.has_default():
-            if callable(self.default):
-                defaults['initial'] = self.default
-                defaults['show_hidden_initial'] = True
-            else:
-                defaults['initial'] = self.get_default()
-        include_blank = self.blank or not (self.has_default() or 'initial' in kwargs)
-        defaults['choices'] = self.get_choices(include_blank=include_blank)
-        defaults['coerce'] = self.to_python
-        if self.null:
-            defaults['empty_value'] = None
-        form_class = SystemViewChoiceField
-        # Many of the subclass-specific formfield arguments (min_value,
-        # max_value) don't apply for choice fields, so be sure to only pass
-        # the values that TypedChoiceField will understand.
-        for k in kwargs.keys():
-            if k not in ('coerce', 'empty_value', 'choices', 'required',
-                         'widget', 'label', 'initial', 'help_text',
-                         'error_messages', 'show_hidden_initial'):
-                del kwargs[k]
-        defaults.update(kwargs)
-        return form_class(**defaults)
