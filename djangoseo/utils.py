49e0a4421100fda6daab9b5034782aa46ca5f799
nitinkondiparthi <nitin.kondiparthi@periship.com>
2022-02-25 15:25:25 -0800

    Change django.core.urlresolvers to django.urls

--- djangoseo/utils.py
+++ djangoseo/utils.py
@@ -6,8 +6,8 @@ from django.utils.functional import lazy
 from django.utils.safestring import mark_safe
 from django.utils.html import conditional_escape
 from django.contrib.contenttypes.models import ContentType
-from django.core.urlresolvers import (RegexURLResolver, RegexURLPattern,
-                                      Resolver404, get_resolver)
+from django.urls import (URLResolver, URLPattern,
+                         Resolver404, get_resolver)
 
 
 class NotSet(object):
@@ -51,9 +51,9 @@ def _resolver_resolve_to_name(resolver, path):
         new_path = path[match.end():]
         for pattern in resolver.url_patterns:
             try:
-                if isinstance(pattern, RegexURLPattern):
+                if isinstance(pattern, URLPattern):
                     name = _pattern_resolve_to_name(pattern, new_path)
-                elif isinstance(pattern, RegexURLResolver):
+                elif isinstance(pattern, URLResolver):
                     name = _resolver_resolve_to_name(pattern, new_path)
             except Resolver404 as e:
                 tried.extend([(pattern.regex.pattern + '   ' + t) for t in
