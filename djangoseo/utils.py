35d3521d4925b873e7c0cbf5eb109be2ebbff805
nitinkondiparthi <nitin.kondiparthi@periship.com>
2022-03-09 11:12:46 -0800

    Change another URLResolver implementation after django upgrade

--- djangoseo/utils.py
+++ djangoseo/utils.py
@@ -56,12 +56,12 @@ def _resolver_resolve_to_name(resolver, path):
                 elif isinstance(pattern, URLResolver):
                     name = _resolver_resolve_to_name(pattern, new_path)
             except Resolver404 as e:
-                tried.extend([(pattern.regex.pattern + '   ' + t) for t in
+                tried.extend([(pattern.pattern.regex.pattern + '   ' + t) for t in
                               e.args[0]['tried']])
             else:
                 if name:
                     return name
-                tried.append(pattern.regex.pattern)
+                tried.append(pattern.pattern.regex.pattern)
         raise Resolver404({'tried': tried, 'path': new_path})
 
 
