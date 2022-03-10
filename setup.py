82f1bac216171f5880be0660d25bd082b7c2db00
yiqing-lan <yiqing.lan@periship.com>
2022-03-09 18:33:18 -0600

    update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = "1.0.1"
+VERSION = "2.0.0"
 
 
 def read_file(filepath):
@@ -9,13 +9,12 @@ def read_file(filepath):
 
 
 setup(
-    name="django-seo2",
+    name="yqnk-django-seo",
     version=VERSION,
     description="A framework for managing SEO metadata in Django.",
     long_description=read_file('README.rst'),
-    url="https://github.com/romansalin/django-seo2",
-    author="Roman Salin",
-    author_email="romansalin1990@gmail.com",
+    url="https://github.com/PeriShipLLC/yqnk-django-seo",
+    author="Nitin Kondiparthi, Yiqing Lan",
     keywords="seo django framework",
     license="MIT",
     include_package_data=True,
@@ -32,12 +31,7 @@ setup(
         "Topic :: Internet :: WWW/HTTP :: Indexing/Search",
         "Topic :: Internet :: WWW/HTTP :: Site Management",
         "Topic :: Software Development :: Libraries :: Application Frameworks",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.8",
     ],
     test_suite="tests.runtests.runtests",
 )
